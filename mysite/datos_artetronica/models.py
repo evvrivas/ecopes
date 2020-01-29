b9292de179d094396266b8f2c915e475ed383a82
evvrivas <evvrivas@gmail.com>
2020-01-28 20:39:39 -0600

    uauauaua

--- mysite/datos_artetronica/models.py
+++ mysite/datos_artetronica/models.py
@@ -67,9 +67,9 @@ class Categoria(models.Model):
          nombre=models.CharField(max_length=30,blank=True,null=True)
                          
          def __str__(self):
-         	return  self.categoria
+         	return  self.nombre
          class Admin:
-         	list_display = ('categoria')
+         	list_display = ('nombre')
 
 
 
